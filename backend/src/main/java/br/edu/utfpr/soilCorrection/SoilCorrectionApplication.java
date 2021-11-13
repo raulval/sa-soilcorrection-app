16ddad61ed901e10c215e5dfef57e0640ef9ac14
Raul Val <raulval@outlook.com>
2021-11-13 13:18:03 -0300

    Inicializa o Back-End

---
+++ backend/src/main/java/br/edu/utfpr/soilCorrection/SoilCorrectionApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.soilCorrection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoilCorrectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoilCorrectionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/edu/utfpr/soilCorrection/SoilCorrectionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.soilCorrection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoilCorrectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
